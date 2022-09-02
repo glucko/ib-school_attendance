df0302ea596720172e35b80965f61d1a7b710d1d
Benjamin Gluck <60188659+glucko@users.noreply.github.com>
2022-09-02 14:30:08 +0000

    fix requirements.txt, add procfile

--- src/cli.py
+++ src/cli.py
@@ -34,6 +34,6 @@ def add_student(name):
 def clear_data(session):
     meta = db.metadata
     for table in reversed(meta.sorted_tables):
-        print 'Clear table %s' % table
+        print('Clear table %s' % table)
         session.execute(table.delete())
     session.commit()
